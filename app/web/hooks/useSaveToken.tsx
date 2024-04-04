e5d7c451cb5c506cbfc7178aa9d9ce251c0b1258
leopham <hongthaipro@gmail.com>
2024-04-04 14:10:11 +0700

    Refactor code to use boxed accounts and update useSaveToken hook

--- app/web/hooks/useSaveToken.tsx
+++ app/web/hooks/useSaveToken.tsx
@@ -1,25 +1,32 @@
 import savePersonToken from '@/actions/savePersonToken';
+import { useMetfProgram } from '@/components/metf/metf-data-access';
 import { useTransactionToast } from '@/components/ui/ui-layout';
+import { BN } from '@coral-xyz/anchor';
 import { useMutation } from '@tanstack/react-query';
 
 const useSaveToken = () => {
   const transactionToast = useTransactionToast();
+  const { createPersonToken } = useMetfProgram();
   return useMutation({
     mutationKey: ['saveToken'],
     mutationFn: async (data: any) => {
-      const formData = new FormData();
-      formData.set('file', data.image);
-      const result = await savePersonToken(
-        {
-          name: data.name,
-          symbol: data.symbol,
-          description: data.description,
-          photo: data.image,
-          model: data.model,
-        },
-        formData
-      );
-      transactionToast('Token saved');
+      // const formData = new FormData();
+      // formData.set('file', data.image);
+      // const result = await savePersonToken(
+      //   {
+      //     name: data.name,
+      //     symbol: data.symbol,
+      //     description: data.description,
+      //     photo: data.image,
+      //     model: data.model,
+      //   },
+      //   formData
+      // );
+      const result = await createPersonToken.mutate({
+        name: data.name,
+        symbol: data.symbol,
+        uri: data.image,
+      });
       return result;
     },
   });
