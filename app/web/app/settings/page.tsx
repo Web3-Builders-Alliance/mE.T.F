af2491fa8198eebfb2a29a7275f81195a8ab407b
Lostin <92709487+lostintime101@users.noreply.github.com>
2024-04-11 22:42:39 +0700

    settings page initial update

--- app/web/app/settings/page.tsx
+++ app/web/app/settings/page.tsx
@@ -1,9 +1,20 @@
 'use client';
 
+import AuthForm from '@/components/login/auth-form';
 import { useMetfProgram } from '@/components/metf/metf-data-access';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 
-const SettingsPage = () => {
+const SettingsPage = async () => {
+  const supabase = createClientComponentClient()
   const { initProgram, createBondcurve } = useMetfProgram();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+  
+  if (!user) return (
+    <AuthForm />
+  );
+  
   return (
     <div>
       <h1>Settings</h1>
